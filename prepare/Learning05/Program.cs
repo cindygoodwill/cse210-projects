516cf794be421dd60d93e86f3698b3fad1db15f3
cindygoodwill <131117504+cindygoodwill@users.noreply.github.com>
2023-05-18 20:00:28 -0600

    Finally finished the learning activity. :)

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> _list = new List<Shape>();
+        
+        Square square = new Square("blue", 5);
+        Rectangle rectangle = new Rectangle("yellow", 8, 7);
+        Circle circle = new Circle("green", 1);
+
+        _list.Add(square);
+        _list.Add(circle);
+        _list.Add(rectangle);
+
+        foreach (Shape shape in _list)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
